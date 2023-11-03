ccd88bc79dde6b63fcf3b493b913fd690309cc90
jefferson.silva <jeff@jeff.com>
2023-11-03 19:14:08 -0300

    container typescript

--- src/infra/web/app.ts
+++ src/infra/web/app.ts
@@ -2,13 +2,17 @@ import 'reflect-metadata';
 import express from 'express';
 import settings from '../config/settings';
 import dynamicModuleLoading from './dynamic-module-loading';
+import { useContainer } from 'class-validator';
+import { TsyringeAdapter } from '../config/container';
+import { container } from 'tsyringe';
 
 const app = dynamicModuleLoading();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+useContainer(new TsyringeAdapter(container))
 
 app.on('error', (error) => console.log({ error }))
 app.listen(settings.PORT, () => {
-  console.log(`App is running on port 8080`);
+  console.log(`App is running on port ${settings.PORT}`);
 });
