0673b7f49af585ca7c3c47a1e8778dd0a4caaa6c
Jefferson Silva <jefferson_1996Th@hotmail.com>
2023-11-04 20:01:01 -0300

    payment types

---
+++ src/domain/enums/payment-types.ts
@@ -0,0 +1,8 @@
+export enum PaymentType {
+    PayPal = 'PayPal',
+    BankTransfer = 'BankTransfer',
+    WesternUnion = 'WesternUnion',
+    MoneyGram = 'MoneyGram',
+    InternationalCreditCard = 'InternationalCreditCard',
+    Other = 'Other',
+}
