9f0e91e26e2c5acd37b47f6490d219f2c6a97138
Jefferson Silva <jefferson_1996Th@hotmail.com>
2023-11-07 14:13:29 -0300

    create user validate address

--- src/adapters/schemas/user/create-user-schema.ts
+++ src/adapters/schemas/user/create-user-schema.ts
@@ -1,4 +1,6 @@
-import { IsNumber, IsString, IsUUID } from "class-validator"
+import { IsDefined, IsNotEmpty, IsNotEmptyObject, IsNumber, IsObject, IsString, IsUUID, ValidateNested } from "class-validator"
+import { UserAddressEntity } from "../../../domain/entities/user_address"
+import { Type } from "class-transformer"
 
 export class CreateUserSchema {
     @IsString()
@@ -21,4 +23,28 @@ export class CreateUserSchema {
 
     @IsString()
     email: string
+
+    @IsDefined()
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => UserAdressSchema)
+    userAddress: UserAddressEntity
+}
+
+class UserAdressSchema {
+    @IsString()
+    addressLine1: string
+
+    @IsString()
+    addressLine2: string
+
+    @IsString()
+    city: string
+
+    @IsString()
+    postalCode: string
+
+    @IsString()
+    country: string
 }
